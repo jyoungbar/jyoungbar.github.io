10c9df804f15b0763ce59dae731c5aa6d2a08789
Josh Youngbar <54415235+jyoungbar@users.noreply.github.com>
2023-07-06 18:38:18 -0400

    created new index.html

--- projects/online_queue/README.md
+++ projects/online_queue/README.md
@@ -9,3 +9,4 @@ https://www.scaler.com/topics/nodejs/socket-io-node-js/
 https://levelup.gitconnected.com/set-up-and-run-a-simple-node-server-project-38b403a3dc09
 https://blog.logrocket.com/deploying-react-apps-github-pages/
 https://github.com/gitname/react-gh-pages
+https://socket.io/docs/v4/client-api/
--- projects/online_queue/server.js
+++ projects/online_queue/server.js
@@ -1,7 +1,23 @@
-//Server
+//Socket.io server
+const express = require('express')
+const PORT = 3000
+const app = express()
+var server = require('http').Server(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+let count = 0;
+io.on('connection', (socket) => {
+  count++;
+  console.log(`${count} user(s) connected`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+});
 
